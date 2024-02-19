ac01724f0eb33f35e882e9b6a1530b6c2f965d41
Alexey Kremnev <alkremn@gmail.com>
2024-02-18 22:39:31 -0700

    end of section 5

--- API/Program.cs
+++ API/Program.cs
@@ -1,26 +1,12 @@
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -43,7 +29,7 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<DataContext>();
-    await context.Database.MigrateAsync();
+    await context.Database.EnsureCreatedAsync();
     await Seed.SeedData(context);
 }
 catch (Exception ex)
